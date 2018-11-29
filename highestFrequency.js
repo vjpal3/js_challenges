224bbeacb8fb2c411d4a2f90739415874e73350c
vjpal3 <vrishalipal@yahoo.com>
2018-11-29 16:59:00 -0500

    most frquent element in array

---
+++ highestFrequency.js
@@ -0,0 +1,28 @@
+function highestFrequency(arr) {
+  const frequencies = {};
+  let maxFrequency = 0;
+  let mostFrequentString = arr[0];
+
+  for(let i=0; i<arr.length; i++) {
+    if(frequencies[arr[i]]) {
+      frequencies[arr[i]] += 1;
+    } else {
+      frequencies[arr[i]] = 1;
+    }
+
+    if(frequencies[arr[i]] > maxFrequency) {
+      maxFrequency = frequencies[arr[i]];
+      mostFrequentString = arr[i]
+    }
+  }
+  return mostFrequentString;
+}
+
+highestFrequency(['a', 'b', 'c', 'ea'])
+=> 'a'
+
+highestFrequency(['a', 'c', 'c', 'b'])
+=> 'c'
+
+highestFrequency(['d', 'b', 'c', 'a'])
+=> 'd'
