43d922bbc6dcc6e957ae8a351d0c02f331519d97
Vrishali Pal <vrishalipal@yahoo.com>
2019-12-12 13:48:01 -0500

    Add functionality to check/uncheck a todo item

--- todo-app/script.js
+++ todo-app/script.js
@@ -5,32 +5,43 @@ const classNames = {
   TODO_DELETE: 'todo-delete',
 }
 
-const list = document.getElementById('todo-list')
-const itemCountSpan = document.getElementById('item-count')
-const uncheckedCountSpan = document.getElementById('unchecked-count')
+const list = document.getElementById('todo-list');
+const itemCountSpan = document.getElementById('item-count');
+const uncheckedCountSpan = document.getElementById('unchecked-count');
 
 function newTodo() {
   let todoItem = prompt("Enter your ToDo here:");
 
   if(todoItem !== null) {
-    var li = document.createElement("li");
+    let li = document.createElement("li");
     li.setAttribute("class", classNames.TODO_ITEM);
 
-    var span = document.createElement("span");
+    let span = document.createElement("span");
     span.setAttribute("class", classNames.TODO_TEXT);
     span.appendChild(document.createTextNode(todoItem));
 
-    var checkbox = document.createElement("input");
+    let checkbox = document.createElement("input");
     checkbox.type = "checkbox";
-    checkbox.name = "name";
-    checkbox.value = "value";
+    checkbox.name = "todoCheckbox";
     checkbox.setAttribute("class", classNames.TODO_CHECKBOX);
+    checkbox.addEventListener("click", setCount);
+    
     li.appendChild(checkbox);
     li.appendChild(span);
     list.appendChild(li);
 
-    var itemCount = Number(itemCountSpan.innerText) + 1;
-    itemCountSpan.innerText = itemCount;
+    itemCountSpan.innerText = Number(itemCountSpan.innerText) + 1;
+    uncheckedCountSpan.innerText = Number(uncheckedCountSpan.innerText) + 1;
   } 
 
+  function setCount(e) {
+    let uncheckedCount = Number(uncheckedCountSpan.innerText);
+    if(e.target.checked) {
+      uncheckedCount--;
+    }
+    else {
+      uncheckedCount++;
+    }
+    uncheckedCountSpan.innerText = uncheckedCount;
+  }
 }
