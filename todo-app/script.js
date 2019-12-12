52b4108ce8c61417875a0c333ba669394c5c645e
Vrishali Pal <vrishalipal@yahoo.com>
2019-12-12 12:36:42 -0500

    Add new Todo and update item count on the page

--- todo-app/script.js
+++ todo-app/script.js
@@ -10,5 +10,27 @@ const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
 function newTodo() {
+  let todoItem = prompt("Enter your ToDo here:");
+
+  if(todoItem !== null) {
+    var li = document.createElement("li");
+    li.setAttribute("class", classNames.TODO_ITEM);
+
+    var span = document.createElement("span");
+    span.setAttribute("class", classNames.TODO_TEXT);
+    span.appendChild(document.createTextNode(todoItem));
+
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.name = "name";
+    checkbox.value = "value";
+    checkbox.setAttribute("class", classNames.TODO_CHECKBOX);
+    li.appendChild(checkbox);
+    li.appendChild(span);
+    list.appendChild(li);
+
+    var itemCount = Number(itemCountSpan.innerText) + 1;
+    itemCountSpan.innerText = itemCount;
+  } 
 
 }
