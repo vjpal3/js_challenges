ebac22786a106180d18e02bacf8812ae3490d2bd
Vrishali Pal <vrishalipal@yahoo.com>
2019-12-17 14:34:15 -0500

    Reverse array in-place using de-structuring assigments

--- Arrays/reverseInPlace.js
+++ Arrays/reverseInPlace.js
@@ -3,7 +3,22 @@ const reverseInPlace = arr => {
         arr.splice(i, 0, arr.pop());
     }
 }
-const arr = [1, 2, 3, 4, 5];
+// const arr = [1, 2, 3, 4, 5];
+// console.log("Array before reversing: ", arr);
+// reverseInPlace(arr);
+// console.log("Array after reversing: ", arr);
+
+const _reverseArray = array => {
+	for (let i = 0; i < array.length / 2; i++) {
+		[array[i], array[array.length - 1 - i]] = [
+			array[array.length - 1 - i],
+			array[i]
+		];
+	}
+	return array;
+};
+
+const arr = [6, 2, 3, 4, 5];
 console.log("Array before reversing: ", arr);
-reverseInPlace(arr);
+_reverseArray(arr);
 console.log("Array after reversing: ", arr);
